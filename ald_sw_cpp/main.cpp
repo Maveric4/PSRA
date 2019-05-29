f4aa149b75a330ba2fa180fee5853c1ba4ed9174
Maveric4 <danielgacek97@gmail.com>
2019-05-29 10:54:38 +0200

    refactorization v2

--- ald_sw_cpp/main.cpp
+++ ald_sw_cpp/main.cpp
@@ -34,6 +34,13 @@ float calculateCommonRectArea(recog_object act_obj, recog_object prev_obj);
 float calculateCommonAreaRatio(float commonArea, recog_object prev_obj);
 float calculateAreaRatio(recog_object act_obj, recog_object prev_obj);
 
+cv::Mat fg_obj_segmentation(cv::Mat &img, cv::Mat &bgModel);
+cv::Mat med_filtr_morph_op(cv::Mat fgMask, cv::Mat kernel);
+cv::Mat mov_obj_segmentation(cv::Mat &img);
+cv::Mat bg_mdl_actualization(cv::Mat &inputImage, cv::Mat &bgModel,cv::Mat &fgMask, cv::Mat &movMask);
+void calc_area_and_ratio_indicators(std::list<recog_object> &act_list, std::list<recog_object> &prev_list);
+void check_for_dang_obj(std::list<recog_object> &prev_list, cv::Mat &imageToShow);
+
 int main() {
     char buffer[100];
     cv::Mat inputImage;
@@ -41,9 +48,7 @@ int main() {
     cv::Mat inputImage_prev;
     cv::Mat movMask;
     cv::Mat bgModel;
-
-    int sum = 0;
-    int mov_sum = 0;
+    cv::Mat fgMask;
 
     cv::Mat labels;
     cv::Mat stats;
@@ -62,84 +67,21 @@ int main() {
             inputImage_prev = inputImage.clone();
         }
 
-        // Foreground ojects segmentation
-        cv::Mat fgMask = cv::Mat::zeros(inputImage.rows, inputImage.cols, CV_8U);
-        for(int jj=0; jj < inputImage.rows; jj++) {
-            for (int ii = 0; ii < inputImage.cols; ii++) {
-                vec_uchar_3 in_pixel = inputImage.at<vec_uchar_3>(jj, ii);
-                vec_uchar_3 m_pixel = bgModel.at<vec_uchar_3>(jj, ii);
-                sum = 0;
-                for (int k = 0; k < 3; k++)
-                    sum += cv::abs(in_pixel[k] - m_pixel[k]);
-
-                if (sum > binThresh)
-                    fgMask.at<uchar>(jj, ii) = 255;
-            }
-        }
+        // Foreground objects segmentation
+        fgMask = fg_obj_segmentation(inputImage, bgModel);
 
         // Median filtration of foreground mask, morphological operations
-//        cv::imshow("Przed", fgMask);
-        cv::medianBlur(fgMask, fgMask, median_ksize);
         cv::Mat kernel = cv::getStructuringElement(cv::MORPH_RECT,  cv::Size(ksize, ksize));
-        cv::morphologyEx(fgMask, fgMask, cv::MORPH_CLOSE, kernel, cv::Point(-1,-1), 3);
-//        cv::morphologyEx(fgMask, fgMask, cv::MORPH_OPEN, kernel);
-//        cv::imshow("Po", fgMask);
+        fgMask = med_filtr_morph_op(fgMask, kernel);
 
         // Moving objects segmentation
-        movMask = cv::Mat::zeros(inputImage.rows, inputImage.cols, CV_8U);
-        for(int jj=0; jj < movMask.rows; jj++) {
-            for (int ii = 0; ii < movMask.cols; ii++) {
-                vec_uchar_3 in_pixel = inputImage.at<vec_uchar_3>(jj, ii);
-                vec_uchar_3 prev_pixel = inputImage_prev.at<vec_uchar_3>(jj, ii);
-                mov_sum = 0;
-                for (int k = 0; k < 3; k++)
-                    mov_sum += cv::abs(in_pixel[k] - prev_pixel[k]);
-
-                if (mov_sum > mov_binThresh){
-                    movMask.at<uchar>(jj, ii) = 255;
-                }
-            }
-        }
+        movMask = mov_obj_segmentation(inputImage);
 
         // Median filtration of moving objects mask, morphological operations
-        cv::medianBlur(movMask, movMask, median_ksize);
-        cv::morphologyEx(movMask, movMask, cv::MORPH_CLOSE, kernel, cv::Point(-1,-1), 3);
+        movMask = med_filtr_morph_op(movMask, kernel);
 
         // Background model actualization
-        for(int jj=0; jj < inputImage.rows; jj++) {
-            for(int ii=0; ii < inputImage.cols; ii++) {
-                vec_uchar_3 in_pixel = inputImage.at<vec_uchar_3>(jj,ii);
-                vec_uchar_3 m_pixel = bgModel.at<vec_uchar_3>(jj,ii);
-                if(fgMask.at<uchar>(jj, ii) == 0 && movMask.at<uchar>(jj, ii) == 0) {
-                    for (int k = 0; k < 3; k++) {
-                        if (in_pixel[k] - m_pixel[k] > 0)
-                            bgModel.at<vec_uchar_3>(jj, ii)[k] += 1;
-                        if (in_pixel[k] - m_pixel[k] < 0)
-                            bgModel.at<vec_uchar_3>(jj, ii)[k] -= 1;
-                    }
-                }
-            }
-        }
-        /*
-        // Indexation ver 1
-        labels_num = cv::connectedComponentsWithStats(fgMask, labels, stats, centroids, 4, CV_16U); //, cv::CCL_DEFAULT );
-
-        for(int i=1; i<stats.rows; i++) {
-            int x = stats.at<int>(cv::Point(0, i));
-            int y = stats.at<int>(cv::Point(1, i));
-            int w = stats.at<int>(cv::Point(2, i));
-            int h = stats.at<int>(cv::Point(3, i));
-            int area = stats.at<int>(cv::Point(4, i));
-            int max = stats.at<int>(cv::Point(5, i));
-            //std::cout << "x=" << x << " y=" << y << " w=" << w << " h=" << h << std::endl;
-            if(area > 300)
-            {
-                cv::Scalar color(255, 0, 0);
-                cv::Rect rect(x, y, w, h);
-                cv::rectangle(inputImage, rect, color);
-            }
-        }
-        */
+        bgModel = bg_mdl_actualization(inputImage, bgModel,fgMask, movMask);
 
         // Indexation - ver 2
         std::vector<std::vector<cv::Point>> contours;
@@ -157,6 +99,7 @@ int main() {
             mc[i] = cv::Point2f( float(mu[i].m10/mu[i].m00) , float(mu[i].m01/mu[i].m00));
         }
 
+        // Add new objects to actual list of objects
         cv::Rect minRect;
         for( size_t i = 0; i < contours.size(); i++ )
         {
@@ -181,56 +124,15 @@ int main() {
 //            cv::putText(imageToShow, "obj", cv::Point(int(mc[i].x), int(mc[i].y)), cv::FONT_HERSHEY_SIMPLEX, 0.2, color);
         }
 
-        std::list<recog_object>::iterator act_obj;
-        std::list<recog_object>::iterator prev_obj;
-
-        for(act_obj=act_list.begin(); act_obj != act_list.end(); act_obj++)
-        {
-            float commonRectArea = 0;
-            float commonAreaRatio = 0;
-            float areaRatio = 0;
-            float max_commonRectArea = 0;
-            std::list<recog_object>::iterator f_obj;
-            bool objectFound = false;
-            for(prev_obj=prev_list.begin(); prev_obj != prev_list.end(); prev_obj++)
-            {
-                commonRectArea = calculateCommonRectArea(*act_obj, *prev_obj);
-                commonAreaRatio = calculateCommonAreaRatio(commonRectArea, *prev_obj);
-                areaRatio = calculateAreaRatio(*act_obj, *prev_obj);
-                if(commonRectArea > max_commonRectArea)
-                    if(areaRatio > areaRatioThresh and commonAreaRatio > commonAreaRatioThresh) {
-                        max_commonRectArea = areaRatio;
-                        f_obj = prev_obj;
-                        objectFound = true;
-                    }
-            }
-            if(objectFound){
-                (*f_obj).area = (*act_obj).area;
-                (*f_obj).rect = (*act_obj).rect;
-                (*f_obj).frameCounter++;
-                (*f_obj).isVisible = true;
-            }
-            if(not objectFound) {
-                prev_list.push_back(*act_obj);
-            }
-        }
+        // Calculate area and ratio indicators
+        calc_area_and_ratio_indicators(act_list, prev_list);
 
         // Check for dangerous objects
-        for(prev_obj=prev_list.begin(); prev_obj != prev_list.end(); prev_obj++)
-        {
-            if((*prev_obj).frameCounter > frameCounterThresh)
-            {
-                std::cout << (*prev_obj).frameCounter << std::endl;
-                cv::Scalar colorR(0, 0, 255);
-                cv::putText(imageToShow, "BOMBA", cv::Point2f((*prev_obj).rect.x,(*prev_obj).rect.y+2*(*prev_obj).rect.height), cv::FONT_HERSHEY_SIMPLEX, 0.5, colorR, 2);
-                cv::rectangle(imageToShow, (*prev_obj).rect, colorR);
-            }
-            if(not (*prev_obj).isVisible)
-                prev_list.erase(prev_obj++);
-        }
+        check_for_dang_obj(prev_list, imageToShow);
 
         // Clone actual objects to previous ones
         inputImage_prev = inputImage.clone();
+
         // Clear list of actual frame objects
         act_list.clear();
 
@@ -249,6 +151,127 @@ int main() {
     }
     cv::destroyAllWindows();
     return 0;
+}
+
+cv::Mat fg_obj_segmentation(cv::Mat &img, cv::Mat &bgModel)
+{
+    cv::Mat fgMask = cv::Mat::zeros(img.rows, img.cols, CV_8U);
+    int sum = 0;
+    for(int jj=0; jj < img.rows; jj++) {
+        for (int ii = 0; ii < img.cols; ii++) {
+            vec_uchar_3 in_pixel = img.at<vec_uchar_3>(jj, ii);
+            vec_uchar_3 m_pixel = bgModel.at<vec_uchar_3>(jj, ii);
+            sum = 0;
+            for (int k = 0; k < 3; k++)
+                sum += cv::abs(in_pixel[k] - m_pixel[k]);
+
+            if (sum > binThresh)
+                fgMask.at<uchar>(jj, ii) = 255;
+        }
+    }
+    return fgMask;
+}
+
+cv::Mat med_filtr_morph_op(cv::Mat mask, cv::Mat kernel)
+{
+    //cv::imshow("Przed", fgMask);
+    cv::medianBlur(mask, mask, median_ksize);
+    cv::morphologyEx(mask, mask, cv::MORPH_CLOSE, kernel, cv::Point(-1,-1), 3);
+    //cv::morphologyEx(fgMask, fgMask, cv::MORPH_OPEN, kernel);
+    //cv::imshow("Po", fgMask);
+    return mask;
+}
+
+cv::Mat mov_obj_segmentation(cv::Mat &img)
+{
+    cv::Mat movMask = cv::Mat::zeros(img.rows, img.cols, CV_8U);
+    for(int jj=0; jj < img.rows; jj++) {
+        for (int ii = 0; ii < img.cols; ii++) {
+            vec_uchar_3 in_pixel = img.at<vec_uchar_3>(jj, ii);
+            vec_uchar_3 prev_pixel = img.at<vec_uchar_3>(jj, ii);
+            int mov_sum = 0;
+            for (int k = 0; k < 3; k++)
+                mov_sum += cv::abs(in_pixel[k] - prev_pixel[k]);
+
+            if (mov_sum > mov_binThresh){
+                movMask.at<uchar>(jj, ii) = 255;
+            }
+        }
+    }
+    return movMask;
+}
+
+cv::Mat bg_mdl_actualization(cv::Mat &inputImage, cv::Mat &bgModel,cv::Mat &fgMask, cv::Mat &movMask)
+{
+    for(int jj=0; jj < inputImage.rows; jj++) {
+        for(int ii=0; ii < inputImage.cols; ii++) {
+            vec_uchar_3 in_pixel = inputImage.at<vec_uchar_3>(jj,ii);
+            vec_uchar_3 m_pixel = bgModel.at<vec_uchar_3>(jj,ii);
+            if(fgMask.at<uchar>(jj, ii) == 0 && movMask.at<uchar>(jj, ii) == 0) {
+                for (int k = 0; k < 3; k++) {
+                    if (in_pixel[k] - m_pixel[k] > 0)
+                        bgModel.at<vec_uchar_3>(jj, ii)[k] += 1;
+                    if (in_pixel[k] - m_pixel[k] < 0)
+                        bgModel.at<vec_uchar_3>(jj, ii)[k] -= 1;
+                }
+            }
+        }
+    }
+    return bgModel;
+}
+
+void calc_area_and_ratio_indicators(std::list<recog_object> &act_list, std::list<recog_object> &prev_list)
+{
+    std::list<recog_object>::iterator act_obj;
+    std::list<recog_object>::iterator prev_obj;
+    for(act_obj=act_list.begin(); act_obj != act_list.end(); act_obj++)
+    {
+        float commonRectArea = 0;
+        float commonAreaRatio = 0;
+        float areaRatio = 0;
+        float max_commonRectArea = 0;
+        std::list<recog_object>::iterator f_obj;
+        bool objectFound = false;
+        for(prev_obj=prev_list.begin(); prev_obj != prev_list.end(); prev_obj++)
+        {
+            commonRectArea = calculateCommonRectArea(*act_obj, *prev_obj);
+            commonAreaRatio = calculateCommonAreaRatio(commonRectArea, *prev_obj);
+            areaRatio = calculateAreaRatio(*act_obj, *prev_obj);
+            if(commonRectArea > max_commonRectArea)
+                if(areaRatio > areaRatioThresh and commonAreaRatio > commonAreaRatioThresh) {
+                    max_commonRectArea = areaRatio;
+                    f_obj = prev_obj;
+                    objectFound = true;
+                }
+        }
+        if(objectFound){
+            (*f_obj).area = (*act_obj).area;
+            (*f_obj).rect = (*act_obj).rect;
+            (*f_obj).frameCounter++;
+            (*f_obj).isVisible = true;
+        }
+        if(not objectFound) {
+            prev_list.push_back(*act_obj);
+        }
+    }
+}
+
+void check_for_dang_obj(std::list<recog_object> &prev_list, cv::Mat &imageToShow)
+{
+    std::list<recog_object>::iterator act_obj;
+    std::list<recog_object>::iterator prev_obj;
+    for(prev_obj=prev_list.begin(); prev_obj != prev_list.end(); prev_obj++)
+    {
+        if((*prev_obj).frameCounter > frameCounterThresh)
+        {
+            std::cout << (*prev_obj).frameCounter << std::endl;
+            cv::Scalar colorR(0, 0, 255);
+            cv::putText(imageToShow, "BOMBA", cv::Point2f((*prev_obj).rect.x,(*prev_obj).rect.y+2*(*prev_obj).rect.height), cv::FONT_HERSHEY_SIMPLEX, 0.5, colorR, 2);
+            cv::rectangle(imageToShow, (*prev_obj).rect, colorR);
+        }
+        if(not (*prev_obj).isVisible)
+            prev_list.erase(prev_obj++);
+    }
 }
 
 static cv::Rect pointSetBoundingRect( const cv::Mat& points , cv::Mat m)
@@ -329,3 +352,26 @@ float calculateAreaRatio(recog_object act_obj, recog_object prev_obj)
 {
     return act_obj.area / prev_obj.area;
 }
+
+
+
+/*
+// Indexation ver 1
+labels_num = cv::connectedComponentsWithStats(fgMask, labels, stats, centroids, 4, CV_16U); //, cv::CCL_DEFAULT );
+
+for(int i=1; i<stats.rows; i++) {
+    int x = stats.at<int>(cv::Point(0, i));
+    int y = stats.at<int>(cv::Point(1, i));
+    int w = stats.at<int>(cv::Point(2, i));
+    int h = stats.at<int>(cv::Point(3, i));
+    int area = stats.at<int>(cv::Point(4, i));
+    int max = stats.at<int>(cv::Point(5, i));
+    //std::cout << "x=" << x << " y=" << y << " w=" << w << " h=" << h << std::endl;
+    if(area > 300)
+    {
+        cv::Scalar color(255, 0, 0);
+        cv::Rect rect(x, y, w, h);
+        cv::rectangle(inputImage, rect, color);
+    }
+}
+*/
